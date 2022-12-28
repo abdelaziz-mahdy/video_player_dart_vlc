d4440034569c2fe40821945f335c5594233bfc38
Abdelaziz Mahdy <zezohassam@gmail.com>
2022-12-28 19:38:35 +0200

    imports for web

---
+++ lib/init_video_player_dart_vlc/init_video_player_dart_vlc.dart
@@ -0,0 +1,3 @@
+
+
+export 'init_video_player_dart_vlc_ffi.dart' if (dart.library.js) 'init_video_player_dart_vlc_web';
--- lib/video_player_dart_vlc.dart
+++ lib/video_player_dart_vlc.dart
@@ -1,5 +1,5 @@
 library video_player_dart_vlc;
 
-export 'init_video_player_dart_vlc.dart';
+export 'init_video_player_dart_vlc/init_video_player_dart_vlc.dart';
 
 export 'video_player_dart_vlc_platform.dart';
